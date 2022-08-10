239cf0acfbc9b70674af1e5c7791443e22267b8f
Sunil-731820 <sunilkumargupta1170@gmail.com>
2022-08-10 14:58:19 +0530

    updated

---
+++ src/main/java/com/java/ds/GreedyAlgorithms/KruskalAlgorithmsForMST.java
@@ -0,0 +1,11 @@
+package com.java.ds.GreedyAlgorithms;
+
+public class KruskalAlgorithmsForMST {
+	
+	public static void main(String[] args) {
+		System.out.println("This is the example of the Kruskal Algorithms");
+		
+	}
+
+	
+}
